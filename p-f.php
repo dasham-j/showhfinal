781ff2395fee3eb36d0c653987a75fa5778b8e12
dasham-j <75601576+dasham-j@users.noreply.github.com>
2023-11-20 04:27:48 +0530

    Add files via upload

---
+++ p-f.php
@@ -0,0 +1,23 @@
+<?php
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+
+$conn = mysqli_connect('127.0.0.1', 'root', 'HELLO@4321','test1');
+
+// get the post records
+$txtNum = $_POST['num'];
+$txtName = $_POST['name'];
+
+
+if(!$conn){
+	echo 'Connection error: ' ;
+}
+
+
+$sql = "INSERT INTO registration VALUES ('$txtNum','$txtName')";
+	if ($conn->query($sql) === TRUE) {
+	echo "Record added!";
+} else {
+	echo "Error:". $conn->error;
+}
+$conn->close();
